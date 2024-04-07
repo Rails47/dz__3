6c859877a7098cb1667b4f7541307320713bdb0e
Rails47 <158601407+Rails47@users.noreply.github.com>
2024-04-07 21:45:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry("microsoft.com");
+                foreach (IPAddress ip in host.AddressList)
+                {
+                    Console.WriteLine(ip);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
